83a2d58e03e787f93c6e7841d66af2e2ab0ef3b2
CHIRANTH RAJU C <chiranthrajuc@gmail.com>
2025-01-22 17:33:16 +0530

    redo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -50,12 +50,14 @@ export default {
   				'5': 'hsl(var(--chart-5))'
   			},
   			sidebar: {
-  				DEFAULT: 'hsl(var(-background))',
-  				foreground: 'hsl(var(-foreground))',
-  				primary: 'hsl(var(-primary))',
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
   				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
   				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(-ring))'
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
   		borderRadius: {
